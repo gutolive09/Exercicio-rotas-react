0aa624c7d9aa66620a440f0291c898608adf971e
gutolive09 <gutolive01@gmail.com>
2023-09-03 11:26:04 -0300

    Rota Home estruturada.

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function index() {
+export default function Home() {
   return (
-    <div>index</div>
+    <main>
+        <h1>HOME</h1>
+    </main>
   )
 }
